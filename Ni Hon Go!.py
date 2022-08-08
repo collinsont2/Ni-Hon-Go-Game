4f4666169a6acd72c7fee80623648d0bd8d86381
collinsont2 <collinsont@stu.otc.school.nz>
2022-08-08 15:03:48 +1200

    added introductions and response lists.

--- Ni Hon Go!.py
+++ Ni Hon Go!.py
@@ -1,14 +1,22 @@
 # Affrimation
 wrong_response = ["Better luck next time!",  "Keep trying!", "Almost!", "Don't give up"]
 right_response = ["Well-done", "Good job!", "Excellent", "Your getting the hang of this!"]
-
-
+phon_alpha_choice = 0
+sub_menu_choice = 0
 # Main menu function for the user to choose hiragana or katakana
 def main_menu():
     """ This function is giving the user a choice of which phonetic alphabet they want to practice. """
+    global phon_alpha_choice
     phon_alpha_choice = input(int("What Japanese alphabet would you like to practice (enter number of choice)? \n 1. Hiragana \n 2. Katakana"))
-
     return
+# Sub menu function of what the user wants to do with chosen alphabet
+def sub_menu():
+    global sub_menu_choice
+    sub_menu_choice = input(int("What would you like to do with this alphabet:"))
+    return
+
+
+
 
 # Introduce the game and ask the user for their name
 print("Welcome to Ni Hon Go!")
